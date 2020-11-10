67a6b2819cca2e22368fe2a25a1b244accca5f16
Filip Jankoski <filip@agmedia.hr>
2020-11-10 20:15:38 +0100

    Adding images.

---
+++ public/images/banners/tal.blade.php
@@ -0,0 +1,310 @@
+@extends('front.layouts.core')
+
+@push('css')
+@endpush
+
+@section ( 'title', 'Landing page')
+
+@section('content')
+
+
+
+    <section id="slider" class="slider-element slider-parallax" style="background: url('images/bcllanding.jpg') center center; background-size: cover;" data-height-xl="670" data-height-lg="600" data-height-md="550" data-height-sm="550" data-height-xs="500">
+        <div class="slider-parallax-inner">
+            <div class="container clearfix">
+                <div class="vertical-middle center">
+
+                    <div class="heading-block nobottomborder center">
+                        <h1 class="white lh1">
+                            Belupo na još višoj razini
+                        </h1>
+                        <span class="white lhspan">Poštovana gđo Kranjčev, dobrodošli na stranice Skladišne logistike!<br /> Za Belupo, pripremili smo ponudu u skladu s potrebama vaših skladišta<br /> <strong>i to u posebnoj akciji s regalnim konstrukcijama, viličarima i peračima podova!</strong><br /> Pogledajte i ponudu na našem webu gdje Vas čeka još puno Toyota viličara za svaku vašu potrebu te puno sjajnih čistača!</span>
+
+                        <a href="http://www.skladisna-logistika.hr/toyota-i-bt-vilicari/" class="btn btn-lg btn-red nomargin ">Pogledajte ponudu</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="  ">
+
+
+        <div class="clearfix"></div>
+    </section>
+    <div class="clearfix"></div>
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+
+            <div class="container clearfix">
+
+
+
+                <div class="col_one_third ">
+
+
+
+                        <h3 class="color">TOYOTA<br> Premium kvaliteta </h3>
+                        <p class="notopmargin"> Kao vodeći proizvođač viličara u svijetu, Toyota svoju pouzdanost, visoku tehnologiju i filozofiju stalnog napretka iz autoindustrije nastavlja i s viličarima. Stoga, uz našu <strong>ekspertizu na području skladišnog poslovanja</strong>, možemo uvijek ponuditi najkvalitetnije rješenje koje odgovara baš Vašim potrebama.</p>
+
+
+                </div>
+
+                <div class="col_one_third">
+
+
+
+                        <h3 class="color">Jamstvo za poslovanje Belupo skladišta na višoj razini </h3>
+                        <p class="notopmargin"> <strong>Iznimno dugačke garancije</strong> uz sigurnost brze reakcije servisera s isključivo <strong>originalnim Toyota dijelovima</strong> obećanje su koje redovno ispunjavamo zahvaljujući premium kvaliteti regala, viličara i perača, kao i stručnosti naših servisera i prodajnog osoblja.</p>
+
+                </div>
+
+                <div class="col_one_third col_last">
+
+
+
+                        <h3 class="color">Varaždinski serviseri posvećeni vašim skladištima </h3>
+                        <p class="notopmargin"> Za potrebe vaših skladišta, imamo čak <strong>2 certificirana servisera i radionu u Varaždinu</strong> koji će osigurati višu razinu poslovanja vaših skladišta. Uz 86% uspjeha popravka pri prvom izlasku, ovo znači <strong>bržu, efikasniju i isplativiju reakciju.</strong></p>
+
+                </div>
+                <div class="clearfix"></div>
+
+            </div>
+
+
+
+
+            <div class="container clearfix" id="st">
+
+                <!--		<div class="heading-block center">
+                                        <h2>This is a Heading Block</h2>
+                                        <span>This is the Sub Title of the Heading Block</span>
+                                    </div> -->
+
+                <div class="tabs side-tabs nobottommargin clearfix" id="tab-6">
+
+                    <ul class="tab-nav tab-nav2 clearfix">
+
+                        <li><a href="#tabs-21">Tko je Skladišna logistika?</a></li>
+                        <li><a href="#tabs-22">Što bi dobio Belupo kao naš klijent?</a></li>
+                        <li><a href="#tabs-23">Iskustvo sa sličnim projektima?</a></li>
+                        <li><a href="#tabs-24">Zašto baš Toyotini viličari?</a></li>
+
+
+                    </ul>
+
+                    <div class="tab-container">
+
+                        <div class="tab-content clearfix" id="tabs-21">
+                            <h3 class="slred">Tko je Skladišna logistika?</h3>
+                            <p>Skladišna logistika nije samo mjesto gdje možete kupiti viličar. Niti je samo mjesto gdje možete kupiti vrhunski viličar. </p>
+                            <p><strong>Skladišna logistika </strong>je mjesto gdje ćete doći do <strong>svog vrhunskog Toyota viličara sigurno, pouzdano i brzo. </strong>Mjesto gdje ćete pronaći<strong> čistače i perače podova te regalne konstrukcije, ali i idejna rješenja s najvećom mogućom kvalitetom. </strong>I, naposljetku, partner koji će uvijek<strong> osigurati brzu i efikasnu intervenciju</strong> što nam je donijelo i priznanja poput<strong> ASEC certifikata za pravovremene dolaske na intervenciju sa smanjenim troškovima reakcije.</strong></p>
+                            <p><strong>SL </strong>je mjesto gdje ćemo vas uvijek uputiti na<strong> najbolje rješenje, </strong>čak i ako to nama znači<strong> manji profit. </strong>Zašto? Jer vjerujemo da samo dugoročnom suradnjom punom povjerenja zaista možemo ispuniti ono u što vjerujemo, a to je<strong> suradnja s klijentima koji vjeruju u kvalitetu jednako kao i mi - </strong>samo na taj način možemo zajedničkim naporima podići<strong> Vaš posao na višu razinu.</strong></p>
+                            <p>Svog se uvjerenja držimo <strong>pružajući znanje</strong> o kvalitetnim rješenjima i nabavljajući vam samo<strong> vrhunske viličare – bili oni novi, rabljeni ili u najmu - čistače i perače podova te regalne konstrukcije.</strong></p>
+                            <p>Naposljetku, kako bismo vam baš svaki put pružili apsolutnu sigurnost, znanje i kvalitetu, za prodaju i najam odabiremo<strong> samo testirane i pouzdane Toyota viličare.</strong></p>
+                        </div>
+                        <div class="tab-content clearfix" id="tabs-22">
+                            <h3 class="slred">Što bi dobio Belupo kao naš klijent?</h3>
+                            <p><strong><strong>Ukratko, potpuno drugačiji pristup suradnji kroz razmišljanje o Vašem poslovanju i Vašim troškovima.</strong></strong></p>
+                            <p>Prije svega, zahvaljujući<strong> ekspertizi naših stručnjaka i kvaliteti naših strojeva, vjerujemo u dugoročnu suradnju. Cijenimo partnerski odnos puno više od jednokratne prodaje. </strong>To znači da ćemo, bez obzira na pitanje kratkoročnog profita za nas, uvijek partnerski raditi na<strong> smanjenju<strong> Vaših troškova kroz kupnju upravo onog viličara kakvog trebate. </strong></strong>To znači i da ćemo Vam uvijek pružati<strong><strong> samo vrhunsku uslugu i znanje te da će Vaši Toyota viličari - bili oni novi, rabljeni ili u najmu - uvijek biti testirani i spremni pružiti maksimum vrhunske kvalitete. </strong></strong></p>
+                            <p>To znači i da će<strong><strong> naša servisna usluga </strong></strong>biti na najvišoj razini - naš uspjeh pri prvoj reakciji, brza intervencija i originalni Toyota dijelovi osiguravaju<strong><strong> manji trošak servisera, puno kraće pauze u radu Vaših strojeva i, stoga, drastično manje gubitke i veće uštede u slučaju kvara.</strong></strong> S našim certificiranim serviserima,<strong><strong> u 86% slučajeva rješavamo probleme pri prvom izlasku na teren, </strong></strong>a uskoro ćemo u poslovanje uvesti napredne tehnološke novitete s kojima će se moći<strong><strong> pratiti baš svaki detalj svake servisne reakcije, kao i stanje strojeva.</strong></strong> </p>
+                            <p>U konačnici, s <strong>regalnim konstrukcijama koje nudimo u partnerstvu s Toyota Material Handling Croatia</strong>, kao i <strong>čistačima i peračima podova</strong>, možemo ponuditi kompletnu uslugu za Vaša skladišta.<strong><br /></strong></p>
+                            <p><strong><strong>Belupo</strong></strong> će, stoga, kao naš klijent, dobiti <strong>partnera potpuno posvećenog njegovim trgovinama</strong> i uspješnom poslovanju s <strong>uvijek pouzdanim visokokvalitetnim strojevima</strong>, ali i stalnu <strong>edukaciju</strong> i poboljšavanje strojeva i rada skladišta za<strong> Vaš posao na još višoj razini.<br /></strong></p>
+                        </div>
+                        <div class="tab-content clearfix" id="tabs-23">
+                            <h3 class="slred">Iskustvo sa sličnim projektima?</h3>
+                            <p><strong>Preko 900 zadovoljnih poduzeća </strong>s kojima smo surađivali<strong> u zadnjih 8 godina </strong>svjedoče o<strong> iznimnoj kvaliteti naše usluge, </strong>kao i strojeva koje nudimo - bili u pitanju Toyota viličari ili regalne konstrukcije, čistači ili perači podova.</p>
+                            <p>Od sličnih projekata, izdvojili bismo suradnju s <span><strong>Quehenbergerom na 57 viličara</strong> za potrebe poput Belupovih. Što se regalnih konstrukcija, ali i općenito idejnih rješenja za skladišta tiče,</span> takav smo posao odradili za <strong>Feroterm i njihovo skladište s čak 18.000 paletnih mjesta</strong>, zatim za poduzeće <strong>Intereuropa s 12.000 paletnih mjesta</strong> te brojne druge klijente.</p>
+                            <p><strong>Ukratko, počevši od pretprodajnog dijela kroz savjete i odabir idealnih rješenja za skladišno poslovanje, manipulaciju robom te utovar i istovar kamiona, pa sve do postprodajne usluge i iznimno kvalitetnog servisa, pokrivamo sve želje i potrebe naših klijenata.</strong></p>
+                        </div>
+                        <div class="tab-content clearfix" id="tabs-24">
+                            <h3 class="slred">Zašto baš Toyotini viličari?</h3>
+                            <p><strong>O kvaliteti Toyote </strong>ne treba trošiti previše riječi -<strong> pouzdanost na najvišoj razini </strong>omogućuje i<strong> smanjenje troškova održavanja u dugom roku. </strong></p>
+                            <p>Kad je u pitanju<strong> usporedba s konkurencijom, Toyota viličari puno se manje kvare, Toyota industrijski motori su iznimno pouzdani, a SAS sistem aktivne stabilnosti pruža sigurnost u radu</strong> koja je jedinstvena na tržištu<strong>.</strong> Zatim,<strong> maksimalno je smanjeno i vrijeme kad je stroj izvan uporabe, serviseri su vrhunski educirani, a logistika rezervnih Toyotinih dijelova omogućuje brzu reakciju pri svakom izazovu - uostalom, 86% uspješnosti reakcije pri prvom izlasku na teren naših servisera</strong> govori sve o<strong> kvaliteti naše postprodajne usluge.</strong> Europski standardi s japanskom kvalitetom ujedno osiguravaju i iznimnu ekološku osviještenost naših strojeva.</p>
+                            <p>Na kraju, sve to<strong> smanjuje rizik, troškove, ozljede na radu i ostale potencijalne probleme u vašem poslovanju.  Pouzdanost koja pruža sigurnost u radu i izvršenje potrebnog posla</strong> bez iznimke, razlozi su iz kojih kvaliteta u dugom roku pruža,<strong> ne samo općenito bolje, sigurnije i isplativije iskustvo rada, </strong>nego i<strong> manje troškove kroz povoljnije održavanje viličara. </strong></p>
+                            <p><strong>Za Belupo to može značiti sigurnost i pouzdanost toka vaših poslovnih procesa kako bismo, partnerskim pristupom, ispunili obećanje brenda Skladišne logistike, a to je vaš posao na višoj razini!</strong><span style="line-height: 1.5em;"> </span></p>
+                        </div>
+
+
+
+
+                    </div>
+
+                </div>
+
+                <div class="divider"></div>
+
+
+
+
+            </div>
+
+            <div class="section nomargin noborder " style="padding: 60px 0;background-color:#eee">
+
+
+
+                <div class="container clear-bottommargin clearfix">
+
+                    <div class="container clearfix bottommargin">
+                        <div class="heading-block center nomargin">
+                            <h2>IZDVOJENO IZ PONUDE</h2>
+                        </div>
+                    </div>
+
+
+
+
+                    <div id="shop" class="owl-carousel products-carousel carousel-widget shop" style="padding-bottom:30px" data-margin="20" data-nav="true" data-pagi="false" data-items-xs="1" data-items-sm="2" data-items-md="2" data-items-lg="3" data-items-xl="3">
+
+
+
+                    @foreach($top_products as $product)
+
+                        @include('front.product.partials.product-category', [
+                                  'product' => $product,
+                                  'link' => route('proizvod', [
+
+                                      'cat' => isset($product->category()->slug) ? $product->category()->slug : '',
+                                      'subcat' => $product->subcategory() ? $product->subcategory()->slug : 'ikoi',
+                                      'prod' => $product->slug
+                                  ])
+                              ])
+                        <!-- .single-product ends -->
+                        @endforeach
+
+
+
+                    </div>
+
+
+                </div>
+            </div>
+
+
+            <div class="section nomargin noborder bgcolor dark" style="padding: 40px 0;">
+                <div class="container clearfix">
+                <div class="heading-block center nobottomborder nobottommargin topmargin-sm" style="padding:0px 4%">
+                    <h2>"Za potpunog idejno rješenje i opremanje Belupo skladišta, nudimo i vrhunske paletne regale u suradnji s Toyota Material Handling Italia."</h2>
+                    <div class="container clearfix center bottommargin-sm topmargin-sm">
+
+                        <a href="http://www.skladisna-logistika.hr/toyota-i-bt-vilicari/" class="btn btn-lg btn-white  ">Pogledajte ponudu</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+            <div class="section nomargin">
+
+
+
+
+                <div class="fslider testimonial testimonial-full" data-animation="fade" data-arrows="false">
+                    <div class="flexslider">
+                        <div class="slider-wrap">
+
+
+
+                            <div class="slide">
+
+                                <div class="testi-content">
+                                    <p>S novim segmentom rabljenih Toyota viličara, uz raniju mogućnost novih viličara te najma, omogućili smo ispunjavanje svake želje i potrebe naših kupaca.</p>
+                                    <div class="testi-meta">
+                                        Davor Pranić
+                                        <span>Direktor / Managing Director, Skladišna logistika d.o.o.</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="slide">
+
+                                <div class="testi-content">
+                                    <p>Dugogodišnje iskustvo u prodaji, najmu i servisu viličara, ali i planiranju organizacije skladišta čime smo se bavili u ranim počecima, omogućuje nam da uvijek nudimo pravi savjet i pronalazimo najbolji mogući viličar za specifične potrebe korisnika naših usluga.</p>
+                                    <div class="testi-meta">
+                                        Goran Kerečin
+                                        <span>Voditelj prodaje / Sales Manager, Skladišna logistika d.o.o.</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="slide">
+
+                                <div class="testi-content">
+                                    <p>Kako bismo olakšali svakodnevne poslove naših korisnika, trudimo se stalno inovirati uslugu i poboljšavati rezultate našeg servisa - o čijoj ekspertnosti dovoljno govori podatak kako u 86% slučajeva sve izazove rješavamo u prvom izlasku na teren.</p>
+                                    <div class="testi-meta">
+                                        Goran Ivančan
+                                        <span>Voditelj postprodaje / Aftersales manager, Skladišna logistika d.o.o.</span>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <div class="container clear-bottommargin topmargin nobottommargin clearfix">
+
+                <div class="container clearfix bottommargin">
+                    <div class="heading-block center nomargin">
+                        <h2>Još par izdvojenih proizvoda</h2>
+                    </div>
+                </div>
+
+
+                <div id="shop2" class="owl-carousel products-carousel carousel-widget shop" style="padding-bottom:30px" data-margin="20" data-nav="true" data-pagi="false" data-items-xs="1" data-items-sm="2" data-items-md="2" data-items-lg="3" data-items-xl="3">
+
+
+
+                @foreach($popular_products as $product)
+
+                    @include('front.product.partials.product-category', [
+                              'product' => $product,
+                              'link' => route('proizvod', [
+
+                                  'cat' => isset($product->category()->slug) ? $product->category()->slug : '',
+                                  'subcat' => $product->subcategory() ? $product->subcategory()->slug : 'ikoi',
+                                  'prod' => $product->slug
+                              ])
+                          ])
+                    <!-- .single-product ends -->
+                    @endforeach
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+        </div>
+
+    </section><!-- #content end -->
+
+
+@endsection
+
+@push('js')
+@endpush

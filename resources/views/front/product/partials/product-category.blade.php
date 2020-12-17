5858e6720bb0c12a30b5748950c0b1bd5a2fd11d
Filip Jankoski <filip@agmedia.hr>
2020-12-17 22:54:10 +0100

    Corrected discount price on fixed discount.

--- resources/views/front/product/partials/product-category.blade.php
+++ resources/views/front/product/partials/product-category.blade.php
@@ -45,9 +45,15 @@
             </ul>
             @if ($product->price!=0)
                 @if ( ! empty($product->action) && empty($product->action->coupon))
-                    <div class="product-price">
-                        <del>{{ number_format($product->price, 2) }}kn</del> <ins> {{ number_format(($product->price - ($product->price * ($product->action->discount / 100))), 2) }}kn</ins>
-                    </div><!-- .price -->
+                    @if ($product->action->discount)
+                        <div class="product-price">
+                            <del>{{ number_format($product->price, 2) }}kn</del> <ins> {{ number_format(($product->price - ($product->price * ($product->action->discount / 100))), 2) }}kn</ins>
+                        </div>
+                    @else
+                        <div class="product-price">
+                            <del>{{ number_format($product->price, 2) }}kn</del> <ins> {{ number_format($product->action->price, 2) }}kn</ins>
+                        </div>
+                    @endif
                 @else
                     <div class="product-price">
                         <ins>{{ number_format($product->price, 2) }}kn</ins>

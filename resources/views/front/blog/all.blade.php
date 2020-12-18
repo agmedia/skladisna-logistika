60c5b26ee6101175e0677744f4d6c1bf8f470056
Filip Jankoski <filip@agmedia.hr>
2020-12-18 18:56:17 +0100

    Corrected news list links.

--- resources/views/front/blog/all.blade.php
+++ resources/views/front/blog/all.blade.php
@@ -5,55 +5,35 @@
 
 @section ( 'title', 'Novosti')
 @section('content')
-
     <section id="page-title" class="page-title-right page-title-mini">
-
         <div class="container clearfix">
-
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="{{ route('index') }}">Naslovnica</a></li>
-
                 <li class="breadcrumb-item active" aria-current="page">Novosti</li>
             </ol>
         </div>
-
     </section>
-
-
     <section  class="page-title-dark" style="position:relative;background-image: url('https://www.skladisna-logistika.hr/media/images/gallery/page/54/onama.jpg'); background-size: cover; padding: 160px 0;background-position:center" >
-
         <div class="bckd" >
             <div class="container clearfix">
                 <h1 class="hbwhite" style="padding: 120px 0">Novosti</h1>
             </div>
-
         </div>
-
-
     </section><!-- #page-title end -->
-
-
     <!-- Content
 ============================================= -->
     <section id="content">
-
         <div class="content-wrap">
-
             <div class="container clearfix">
-
                 <!-- Posts
                 ============================================= -->
                 <div id="posts" class="small-thumbs">
-
-
-                        @foreach($blogs as $blog)
-
-
+                    @foreach($blogs as $blog)
                         <div class="entry clearfix">
                             @if($blog->image)
-                            <div class="entry-image">
-                                <a href="{{ asset($blog->image) }}" data-lightbox="image"><img class="image_fade" src="{{ asset($blog->image) }}" alt="{{ $blog->title }}"></a>
-                            </div>
+                                <div class="entry-image">
+                                    <a href="{{ asset($blog->image) }}" data-lightbox="image"><img class="image_fade" src="{{ asset($blog->image) }}" alt="{{ $blog->title }}"></a>
+                                </div>
                             @endif
                             <div class="entry-c">
                                 <div class="entry-title">
@@ -69,29 +49,21 @@
                                 </ul>
                                 <div class="entry-content">
                                     <p>{{ $blog->meta_description }}</p>
-                                    <a href="{{ route('blogovi', ['blog' => $blog]) }}"class="more-link">Opširnije</a>
+                                    @if (isset($blog->subcat))
+                                        <a href="{{ route('blogovi', ['cat' => $blog->cat->slug, 'subcat' => $blog->subcat->slug, 'blog' => $blog->slug]) }}" class="more-link">Opširnije</a>
+                                    @else
+                                        <a href="{{ route('blogovi', ['cat' => $blog->cat->slug, 'subcat' => $blog->slug]) }}" class="more-link">Opširnije</a>
+                                    @endif
                                 </div>
                             </div>
                         </div>
-
                     @endforeach
-
-
-
                     {{ $blogs->links('front.layouts.partials.paginate') }}
-
-
-
                 </div>
-
             </div>
-
         </div>
-
     </section><!-- #content end -->
-
 @endsection
 
 @push('js_after')
-
 @endpush

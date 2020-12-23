a42be38b7f763c35dd6728162cb322eea762765f
Filip Jankoski <filip@agmedia.hr>
2020-12-23 19:20:29 +0100

    Removed Google preview from manufacturer edit.

--- resources/views/back/catalog/manufacturer/edit.blade.php
+++ resources/views/back/catalog/manufacturer/edit.blade.php
@@ -96,27 +96,6 @@
                                 <label for="slug">Svojevoljni SEO URL <span class="text-gray">Nije preporučljivo!</span></label>
                                 <input type="text" class="form-control" name="slug" id="slug-input" value="{{ isset($manufacturer->slug) ? $manufacturer->slug : '' }}" placeholder="">
                             </div>
-
-                            <div class="block block-mode-hidden mb-30 mt-20 d-none d-md-block">
-                                <div class="block-header block-header-default" style="border: 1px solid #e9e9e9;">
-                                    <h3 class="block-title">Google pregled</h3>
-                                    <div class="block-options">
-                                        <button type="button" class="btn-block-option" data-toggle="block-option" data-action="content_toggle"><i class="si si-arrow-up"></i></button>
-                                    </div>
-                                </div>
-                                <div class="block-content">
-                                    <div class="form-group">
-                                        <div class="block border">
-                                            <div class="block-content pt-10">
-                                                <p id="category-title-value" class="lead font-w400 mb-0" style="color: blue;"></p>
-                                                <p id="category-url-value" class="mb-0 font-w300" style="color: green;"></p>
-                                                <p id="category-content-value" class=""></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
 
                         <div class="col-lg-5">
@@ -205,11 +184,8 @@
                     ['view', ['codeview', 'help']]
                 ],
                 styleTags: ['p', 'h4', 'blockquote'],
-            })
-
-            SetSEOPreview()
-        })
-
+            });
+        });
 
         function setImage(e) {
             let file = e.target.files[0]
@@ -223,22 +199,6 @@
             }
 
             reader.readAsDataURL(file)
-        }
-
-
-        function SetSEOPreview() {
-            let title = document.getElementById('category-name-input').value
-            document.getElementById('slug-input').value = slugify(title)
-
-            if (title) {
-                document.getElementById('category-title-value').innerHTML = title
-                document.getElementById('category-url-value').innerHTML = 'https://{{ request()->getHost() }}/' + slugify(title)
-            }
-
-            let category_meta_description = document.getElementById('category-meta-description').value
-            if (category_meta_description) {
-                document.getElementById('category-content-value').innerHTML = category_meta_description
-            }
         }
 
     </script>

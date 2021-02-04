b65c5b4c109bc723678867238b0acaaa39a5bf19
Filip Jankoski <filip@agmedia.hr>
2021-02-04 14:25:57 +0100

    Fixed front sidebar navigation.

--- resources/views/front/layouts/partials/sidenav.blade.php
+++ resources/views/front/layouts/partials/sidenav.blade.php
@@ -1,10 +1,10 @@
 <nav id="menu">
     <ul>
-        <li ><a href="{{ route('gcp_route', ['group' => 'toyota-vilicari']) }}" >TOYOTA viličari</a></li>
-        <li ><a href="{{ route('tal') }}" ><div>TAL (Toyota advanced Logistics)</div></a></li>
-        <li ><a  href="{{ route('o-nama') }}" ><div>O nama</div></a></li>
-        <li ><a href="{{ route('servis') }}" >Servis viličara</a></li>
-        <li class="last"><a  href="{{ route('kontakt') }}" ><div>Kontakt</div></a></li>
+        <li><a href="{{ route('gcp_route', ['group' => 'toyota-vilicari']) }}">TOYOTA viličari</a></li>
+        <li><a href="{{ route('tal') }}"><div>Logistička rješenja</div></a></li>
+        <li><a href="{{ route('rabljeno') }}">Rabljeno</a></li>
+        <li><a href="{{ route('o-nama') }}"><div>O nama</div></a></li>
+        <li class="last"><a href="{{ route('kontakt') }}"><div>Kontakt</div></a></li>
     </ul>
 </nav>
 
